1fd0e07c5067da90f9b58e2a7fbcb8eea2a923d3
Drew-Trumbaturi <30042619@student.toiohomai.ac.nz>
2022-06-13 15:33:29 +1200

    added images

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,160 +3,158 @@ import "./Decor.css";
 import Image from "../images/Logo.png";
 import Hamburger from "./Hamburger";
 
-function Nav() 
-  {
-    const [hamburgerOpen,setHamBurgerOpen] = useState(false);
-    const toggleHamburger = () =>{
-      setHamBurgerOpen(!hamburgerOpen)
-    }
+function Nav() {
+  // const [hamburgerOpen,setHamBurgerOpen] = useState(false);
+  // const toggleHamburger = () =>{
+  //   setHamBurgerOpen(!hamburgerOpen)
+  // }
   return (
     <div>
-    <nav
-      className="navbar sticky-top navbar-expand-lg navbar-dark navnav justify-content-center"
-      style={{ padding: 3 }}>
-      <div className="row container-fluid">
-        <div className="col-2">
-          <div id="wrapper">
-            <Link to="/" className="text-white text-decoration-none">
-              <div className="row">
-                <div className="col-sm-1">
-                  <img
-                    src={Image}
-                    style={{ height: "50px", width: "50px" }}
-                    alt="Logo"
+      <nav
+        className="navbar sticky-top navbar-expand-lg navbar-dark navnav justify-content-center"
+        style={{ padding: 3 }}>
+        <div className="row container-fluid">
+          <div className="col-2">
+            <div id="wrapper">
+              <Link to="/" className="text-white text-decoration-none">
+                <div className="row">
+                  <div className="col-sm-1">
+                    <img
+                      src={Image}
+                      style={{ height: "50px", width: "50px" }}
+                      alt="Logo"
+                    />
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="d-flex justify-content-center">
+              <ul className="nav">
+                <Link
+                  to="/"
+                  className="nav-item"
+                  style={{ textDecoration: "none" }}>
+                  <li className="nav-link text-light">Home</li>
+                </Link>
+                <Link
+                  to="#"
+                  className="nav-item"
+                  style={{ textDecoration: "none" }}>
+                  <li className="nav-link text-light">About</li>
+                </Link>
+                <Link
+                  to="/Create"
+                  className="nav-item"
+                  style={{ textDecoration: "none" }}>
+                  <li className="nav-link text-light">Submit SCP</li>
+                </Link>
+                <div>
+                  <li className="nav-item dropdown">
+                    <li
+                      className="nav-link dropdown-toggle text-decoration-none text-light"
+                      xhref="#"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      Catalog
+                    </li>
+                    <ul
+                      className="dropdown-menu dropdown-menu-dark"
+                      aria-labelledby="navbarDropdown">
+                      <label className="dropdown-item disabled">
+                        Our Top Five Picks
+                      </label>
+                      <li>
+                        <hr className="dropdown-divider" />
+                      </li>
+                      <Link
+                        to="/scp/629ed30f548c12b728568243"
+                        className="text-decoration-none">
+                        <li className="dropdown-item" xhref="#">
+                          SCP-049
+                        </li>
+                      </Link>
+                      <li className="dropdown-item" xhref="#">
+                        SCP-169
+                      </li>
+                      <li className="dropdown-item" xhref="#">
+                        SCP-420
+                      </li>
+                      <li className="dropdown-item" xhref="#">
+                        SCP-957
+                      </li>
+                      <li className="dropdown-item" xhref="#">
+                        SCP-999
+                      </li>
+                      <li>
+                        <hr className="dropdown-divider" />
+                      </li>
+                      <Link className="text-decoration-none" to="/recordList">
+                        <li className="dropdown-item">All SCPs</li>
+                      </Link>
+                      <li className="dropdown-item" xhref="#">
+                        Today's SCP Picks
+                      </li>
+                      <li className="dropdown-item" xhref="#">
+                        New SCP Entries
+                      </li>
+                      <li className="dropdown-item" xhref="#">
+                        Random SCP
+                      </li>
+                    </ul>
+                  </li>
+                </div>
+              </ul>
+            </div>
+          </div>
+          <div className="col-4 d-flex">
+            <div id="wrapper" className="justify-content-end">
+              <div className="row g-2">
+                <div className="col-10 searchBox">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Search"
                   />
                 </div>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className="col-6">
-          <div className="d-flex justify-content-center">
-            <ul className="nav">
-              <Link
-                to="/"
-                className="nav-item"
-                style={{ textDecoration: "none" }}>
-                <li className="nav-link text-light">Home</li>
-              </Link>
-              <Link
-                to="#"
-                className="nav-item"
-                style={{ textDecoration: "none" }}>
-                <li className="nav-link text-light">About</li>
-              </Link>
-
-              <Link
-                to="/Create"
-                className="nav-item"
-                style={{ textDecoration: "none" }}>
-                <li className="nav-link text-light">Submit SCP</li>
-              </Link>
-              <div>
-                <li className="nav-item dropdown">
-                  <li
-                    className="nav-link dropdown-toggle text-decoration-none text-light"
-                    xhref="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    Catalog
-                  </li>
-                  <ul
-                    className="dropdown-menu dropdown-menu-dark"
-                    aria-labelledby="navbarDropdown">
-                    <label className="dropdown-item disabled">
-                      Our Top Five Picks
-                    </label>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li className="dropdown-item" xhref="#">
-                      SCP-049
-                    </li>
-                    <li className="dropdown-item" xhref="#">
-                      SCP-169
-                    </li>
-                    <li className="dropdown-item" xhref="#">
-                      SCP-420
-                    </li>
-                    <li className="dropdown-item" xhref="#">
-                      SCP-999
-                    </li>
-                    <li className="dropdown-item" xhref="#">
-                      SCP-957
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <Link className="text-decoration-none" to="/recordList">
-                      <li className="dropdown-item">All SCPs</li>
-                    </Link>
-                    <li className="dropdown-item" xhref="#">
-                      Today's SCP Picks
-                    </li>
-                    <li className="dropdown-item" xhref="#">
-                      New SCP Entries
-                    </li>
-                    <li className="dropdown-item" xhref="#">
-                      Random SCP
-                    </li>
-                  </ul>
-                </li>
-              </div>
-            </ul>
-          </div>
-        </div>
-        <div className="col-4 d-flex">
-          <div id="wrapper" className="justify-content-end">
-            <div className="row g-2">
-              <div className="col-10 searchBox">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Search"
-                />
-              </div>
-              <div className="col-2">
-                <button
-                  type="submit"
-                  className="btn btn-danger btn-outline-light">
-                  Submit
-                </button>
+                <div className="col-2">
+                  <button
+                    type="submit"
+                    className="btn btn-danger btn-outline-light">
+                    Submit
+                  </button>
+                </div>
               </div>
             </div>
           </div>
+          <div className="hamburger">
+            <Hamburger />
+          </div>
         </div>
-        <div className="hamburger">
-          <Hamburger />
-        </div>
-      </div>
-    </nav>
-    <style jsx>{`
-    .navigation{
-      width:100%;
-      height:50px;
+      </nav>
+      <style jsx>{`
+        .navigation {
+          width: 100%;
+          height: 50px;
+        }
+        .navigation ul {
+          display: flex;
+          flex-wrap: wrap;
+          float: right;
+          margin: 20 0px;
+          padding: 0 25px;
+        }
 
-    }
-    .navigation ul{
-      display:flex;
-      flex-wrap:wrap;
-      float:right;
-      margin:20 0px;
-      padding: 0 25px;
-    }
-    
-    .navigation ul li{
-      list-style-type:none;
-      padding-right: 10px;
-    }
-    
-  `}</style>
+        .navigation ul li {
+          list-style-type: none;
+          padding-right: 10px;
+        }
+      `}</style>
     </div>
   );
 }
 
 export default Nav;
-
-
--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -27,36 +27,38 @@ export default function RecordList() {
   // This method will map out the records on the table
   function recordList() {
     return records.map((scp) => {
-      return(
-      <div key={scp._id} className="col-6 p-2 bg-light">
-          
-            <div className="card bg-dark text-light">
-              <Link to={`/scp/${scp._id}`}>
-                <img src={scp.image} className="card-img-top" alt="scp" />
-              </Link>
+      return (
+        <div key={scp._id} className="col-6 p-2 bg-light">
+          <div className="card bg-dark text-light">
+            <Link to={`/scp/${scp._id}`}>
+              <img
+                src={`../images/${scp.image}`}
+                className="card-img-top"
+                alt="scp-pictures"
+              />
+            </Link>
+            <hr />
+            <div className="card-body p-2">
+              <h4 className="card-title">{scp.name}</h4>
+              <p className="card-title">Item #: {scp.item}</p>
+              <p className="card-text">Objectclass: {scp.objectclass}</p>
               <hr />
-              <div className="card-body p-2">
-                <h4 className="card-title">{scp.name}</h4>
-                <p className="card-title">Item #: {scp.item}</p>
-                <p className="card-text">Objectclass: {scp.objectclass}</p>
-                <hr />
-                <p className="card-text">
-                  Description:{" "}
-                  {scp.description.length > 250
-                    ? `${scp.description.substring(0, 200)}...`
-                    : scp.description}
-                </p>
-                <Link to={`/scp/${scp._id}`}>
-                  <button
-                    type="button"
-                    className="text-light btn btn-primary btn-outline-secondary">
-                    More info
-                  </button>
-                </Link>
-              </div>
+              <p className="card-text">
+                Description:{" "}
+                {scp.description.length > 250
+                  ? `${scp.description.substring(0, 200)}...`
+                  : scp.description}
+              </p>
+              <Link to={`/scp/${scp._id}`}>
+                <button
+                  type="button"
+                  className="text-light btn btn-primary btn-outline-secondary">
+                  More info
+                </button>
+              </Link>
             </div>
           </div>
-
+        </div>
       );
     });
   }
@@ -65,14 +67,10 @@ export default function RecordList() {
   return (
     <div className="container text-light">
       <h3>SCP Catalog List</h3>
-      
-      
+
       <div className="p-3">
-      
-      <div className="row mt-3 mb-3">{recordList()}</div>
+        <div className="row mt-3 mb-3">{recordList()}</div>
       </div>
     </div>
-
-    
   );
 }
