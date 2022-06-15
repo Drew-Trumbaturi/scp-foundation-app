5512e505234facf003e78925320c437e97cf4554
Drew-Trumbaturi <30042619@student.toiohomai.ac.nz>
2022-06-15 22:27:42 +1200

    Added Cors

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({ origin: "https://scp-foundation-app.netlify.app" }));
 app.use(express.json());
 app.use(require("./routes/record"));
 // Get driver connection
